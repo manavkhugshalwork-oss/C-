055bfaa9f9e88b91382a90fdeb061fe307ecbead
Manav <manavkhugshalwork@gmail.com>
2026-09-10 20:05:17 +0530

    Program to find sum of n natural numbers

---
+++ 4-Sum_of_N_natural_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, sum;
+    cout << "Enter the number : ";
+    cin >> n;
+
+    sum = (n * (n + 1)) / 2;
+    cout << "The sum of n natural numbers is : " << sum;
+    return 0;
+}
