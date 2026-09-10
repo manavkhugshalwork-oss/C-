7b09b866d0076c4e91cd423e29e760f6f8a30ca1
Manav <manavkhugshalwork@gmail.com>
2026-09-10 20:35:19 +0530

    Program to find the area of a circle

---
+++ 6-Area_of_a_circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r, area;
+    cout << "Enter the radius of the circle : ";
+    cin >> r;
+
+    area = float(22/7) * r * r;
+    cout << "The area of the circle is : " << area;
+    return 0;
+}
