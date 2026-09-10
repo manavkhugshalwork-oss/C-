e9b46fefdaac335b9123db5f4f55810274f702bd
Manav <manavkhugshalwork@gmail.com>
2026-09-11 01:51:50 +0530

    Program to calculate Simple interest

---
+++ 8-Simple_Interest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float p, r, t, si;
+    cout << "Enter the principle amount :";
+    cin >> p;
+    cout << "Enter the rate of interest :";
+    cin >> r;
+    cout << "Enter the time period :";
+    cin >> t;
+
+    si = (p * r * t) / 100;
+    cout << "The simple interest is : " << si;
+    return 0;
+}
