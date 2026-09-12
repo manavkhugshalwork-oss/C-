7ee537277f27d39638a206c5887309eb3033628a
Manav <manavkhugshalwork@gmail.com>
2026-09-12 23:53:47 +0530

    Program to find distance between 2 points

---
+++ 10-Distance_between_2_points.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float x1, x2, y1, y2, d;
+    cout << "Enter the coordinates of 1st point : ";
+    cin >> x1 >> y1;
+    cout << "Enter the coordinates of 2nd point : ";
+    cin >> x2 >> y2;
+
+    d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    cout << "The distance between the points is : " << d;
+    return 0;
+}
