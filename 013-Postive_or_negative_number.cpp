e93ecfd527d5dadf54d99a7148ed6fbdc687f504
Manav <manavkhugshalwork@gmail.com>
2026-09-15 23:44:45 +0530

    Program to check even/odd number

---
+++ 013-Postive_or_negative_number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number : ";
+    cin>>x;
+    if(x>=0){
+        cout<<"The number is postive";
+    }
+    else{
+        cout<<"The number is negative";
+    }
+    return 0;
+}
